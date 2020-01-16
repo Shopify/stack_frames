9bb1fa48dddba7a23e797d18f2714b50ae224f83
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2020-01-16 10:38:12 -0500

    Avoid turning compiler warnings into errors outside of development

--- ext/stack_frames/extconf.rb
+++ ext/stack_frames/extconf.rb
@@ -1,3 +1,4 @@
 require 'mkmf'
-$CFLAGS << ' -Wall -Werror -Wextra -Wno-unused-parameter -Wno-missing-field-initializers'
+$CFLAGS << ' -Wall -Wextra -Wno-unused-parameter -Wno-missing-field-initializers'
+$CFLAGS << ' -Werror' if ENV['STACK_FRAMES_DEV']
 create_makefile("stack_frames/stack_frames")
