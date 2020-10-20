ffb952704d4077519e0ccd52ad60eb47192fb3e5
Dylan Thacker-Smith <dylan.smith@shopify.com>
2020-10-19 21:20:45 -0400

    Use rb_global_variable to avoid problems with GC.compact (#10)

--- ext/stack_frames/stack_frames.c
+++ ext/stack_frames/stack_frames.c
@@ -2,10 +2,8 @@
 #include "frame.h"
 #include "buffer.h"
 
-VALUE mStackFrames;
-
 void Init_stack_frames() {
-    mStackFrames = rb_define_module("StackFrames");
+    VALUE mStackFrames = rb_define_module("StackFrames");
     stack_buffer_define(mStackFrames);
     stack_frame_define(mStackFrames);
 }
