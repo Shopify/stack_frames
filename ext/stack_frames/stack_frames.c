055ce210c7001f37bf9bc870f860103b336bcce5
Jean Boussier <jean.boussier@gmail.com>
2023-03-01 18:18:54 +0100

    Fix old-style-definition warning

--- ext/stack_frames/stack_frames.c
+++ ext/stack_frames/stack_frames.c
@@ -2,7 +2,7 @@
 #include "frame.h"
 #include "buffer.h"
 
-RUBY_FUNC_EXPORTED void Init_stack_frames() {
+RUBY_FUNC_EXPORTED void Init_stack_frames(void) {
     VALUE mStackFrames = rb_define_module("StackFrames");
     stack_buffer_define(mStackFrames);
     stack_frame_define(mStackFrames);
